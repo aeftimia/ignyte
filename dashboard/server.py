fc9cdac4e0af3d5a331c70427faeeb733f2e0054
Alex Eftimiades <alexeftimiades@gmail.com>
2019-04-09 11:39:13 -0400

    display all machines per location

--- dashboard/server.py
+++ dashboard/server.py
@@ -14,18 +14,25 @@ PORT_NUMBER = 8080
 #This class will handles any incoming request from
 #the browser 
 
-def format_popup(machine_id, location, readings):
-    ret = f'{location}<br>Machine: {machine_id}\n'
-    ret += '<ul>\n'
-    abnormal = False
-    for key, value in readings.items():
-        item = f'{key}: {value:.2f}'
-        if value > 0.5:
-            abnormal = True
-            item = f'<font color="red">{item}</font>'
-        ret += f'<li>{item}</li>\n'
+def format_popup(machines):
+    ret = '<ul>\n'
+    any_abnormal = False
+    for machine, readings in machines:
+        abnormal = False
+        machine = f'{machine}'
+        data = ''
+        for key, value in zip(readings.sensor_type, readings.sensor_value):
+            item = f'{key}: {value:.2f}'
+            if value > 0.5:
+                abnormal = True
+                any_abnormal = True
+                item = f'<font color="red">{item}</font>'
+            data += f'<li>{item}</li>'
+        if abnormal:
+            machine = f'<font color="red">{machine}</font>'
+        ret += f'<li>{machine}<ul>{data}</ul></li>\n'
     ret += '</ul>'
-    return ret, abnormal
+    return ret, any_abnormal
 
 class myHandler(BaseHTTPRequestHandler):
     
@@ -36,11 +43,10 @@ class myHandler(BaseHTTPRequestHandler):
         self.end_headers()
         # Send the html message
         records = []
-        for sensor_id, group in pandas.read_sql('select * from master order by created_at desc;', engine).drop_duplicates('sensor_id').groupby('machine_id'):
+        for location, group in pandas.read_sql('select * from master order by created_at desc;', engine).drop_duplicates('sensor_id').groupby('location'):
             record = json.loads(group.iloc[0].to_json())
-            record['reading'], record['abnormal'] = format_popup(record.pop('machine_id'),
-                    record.pop('location'),
-                    dict(zip(group.sensor_type, group.sensor_value)))
+            record['reading'], record['abnormal'] = format_popup(group.groupby('machine_id'))
+            record['reading'] = f'{location}<br>\n' + record['reading']
             record.pop('sensor_type')
             record.pop('sensor_value')
             records.append(record)
