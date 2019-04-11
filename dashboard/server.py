9f3f6fdda8de66990d56390de141ec135187d2ad
Alex Eftimiades <alexeftimiades@gmail.com>
2019-04-11 08:40:54 -0400

    serve map and chart--local js files not loading

--- dashboard/server.py
+++ dashboard/server.py
@@ -15,6 +15,9 @@ PORT_NUMBER = 8080
 #This class will handles any incoming request from
 #the browser 
 
+def is_abmormal(value):
+    return value > 0.5
+
 def format_popup(machines):
     ret = '<ul>\n'
     any_abnormal = False
@@ -25,7 +28,7 @@ def format_popup(machines):
         data = ''
         for key, value in zip(readings.sensor_type, readings.sensor_value):
             item = f'{key}: {value:.2f}'
-            if value > 0.5:
+            if is_abmormal(value):
                 abnormal = True
                 any_abnormal = True
                 item = f'<font color="red">{item}</font>'
@@ -51,16 +54,17 @@ class myHandler(BaseHTTPRequestHandler):
 
     def send_chart(self):
         records = []
-        for location, group in pandas.read_sql('select * from master order by created_at desc;', engine).drop_duplicates('sensor_id').groupby('location'):
-            record = json.loads(group.iloc[0].to_json())
-            record['reading'], record['abnormal'] = format_popup(group.groupby('machine_id'))
-            record['reading'] = f'{location}<br>\n' + record['reading']
-            record.pop('sensor_type')
-            record.pop('sensor_value')
-            records.append(record)
-        with open(os.path.join(here, 'map.html'), 'r') as f:
+        data = pandas.read_sql('select * from master order by created_at, location desc;', engine)
+        data['abnormal'] = data.sensor_value.map(is_abmormal)
+        x = ['x',] + data.created_at.apply(lambda x: x.strftime('%B %d, %Y, %r')).values.tolist()
+        print(x)
+        columns = [x]
+        for location, group in data.groupby('location'):
+            column = [location,] + group.groupby('created_at').abnormal.agg('mean').values.tolist()
+            columns.append(column)
+        with open(os.path.join(here, 'chart.html'), 'r') as f:
             response = f.read()
-        self.wfile.write(macro_expand(response, readings=json.dumps(records)).encode())
+        self.wfile.write(response.encode())#, columns=json.dumps(columns)).encode())
 
 
     def send_map(self):
