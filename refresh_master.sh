a28a15228cf1ed1a8460389c834de4f141b43413
Alex Eftimiades <alexeftimiades@gmail.com>
2019-04-12 12:33:23 -0400

    add host commandline argument

--- refresh_master.sh
+++ refresh_master.sh
@@ -1,5 +1,5 @@
 while true
 do
-    PGPASSWORD=$ignyte_db_password psql -U $ignyte_db_username $ignyte_db -c "select master_refresh();";
+    PGPASSWORD=$ignyte_db_password psql -U $ignyte_db_username $ignyte_db -h localhost -c "select master_refresh();";
     sleep 45;
 done
--- sensor_reading.sh
+++ sensor_reading.sh
@@ -1,5 +1,5 @@
 while true
 do
-    PGPASSWORD=$ignyte_db_password psql -U $ignyte_db_username $ignyte_db -c "select record();"
+    PGPASSWORD=$ignyte_db_password psql -U $ignyte_db_username -h localhost $ignyte_db -c "select record();"
     sleep 30;
 done
